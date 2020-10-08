b15362abbd812350088cba690e46858468d14653
yogi13995 <ycchoudhary13995@gmail.com>
2020-10-08 23:35:41 +0530

    changes in latex file

--- octave/frequency/comparision/frequency.py
+++ octave/frequency/comparision/frequency.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-x1 = np.loadtxt('frequency1.dat', dtype = 'double')
-x2 = np.loadtxt('frequency.dat', dtype = 'double')
+x1 = np.loadtxt('frequency.dat', dtype = 'double')
+x2 = np.loadtxt('frequency2.dat', dtype = 'double')
 
 A1= []
 estimate1 = []
-for i in range(0,18):
+for i in range(0,50):
 	A1.append(x1[i])
 	
 	
-for i in range(0,18):
-	estimate1.append(x1[i+18])
+for i in range(0,50):
+	estimate1.append(x1[i+50])
 	
 A2= []
 estimate2 = []
@@ -27,8 +27,8 @@ for i in range(0,50):
 
 
 plt.grid(True, which = "both")
-plt.plot(A1,estimate1, label= 'with pilot = 18,delf = 5e6')
-plt.plot(A2,estimate2, label= 'with pilot = 50,delf = 5e4')
+plt.plot(A1,estimate1, label= 'for m <= 10')
+plt.plot(A2,estimate2, label= 'for m > 10')
 plt.ylabel('$estimate$')
 plt.xlabel('$A$')
 plt.legend()
